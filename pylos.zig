b08b650763dd407bd6ff5302411587132629b4f6
Jean-Marc Alliot <jm@alliot.org>
2024-12-03 16:02:47 +0200

    Everything's changed

--- pylos.zig
+++ pylos.zig
@@ -1,85 +1,83 @@
 const std = @import("std");
 
+const stdin = std.io.getStdIn().reader();
+const stdout = std.io.getStdOut().writer();
+const stderr = std.io.getStdErr().writer();
+
+const USE_BMOVE: bool = false; // Looks like, for finding the shortest solution, it is better not to use bmove...
+
 // 27 bits use 2GB
 const NB_BITS: u8 = 25;
-const SIZEX: usize = 4;
-const SIZEY: usize = 4;
-const SIZEZ: usize = 4;
-// 6x7 NB_BITS=29 170s
-// 7x6 NB_BITS=30 367s
 
 const Vals = i16;
-const Vals_min: Vals = std.math.minInt(i8);
-const Vals_max: Vals = std.math.maxInt(i8);
-const WIN: Vals = Vals_max - 1;
+const Vals_min: Vals = std.math.minInt(Vals);
+const Vals_max: Vals = std.math.maxInt(Vals);
 const Depth = u8;
-const Colors = i8;
+const Colors = u1;
 const Sigs = u64;
+// bit  0:15 level 0 white, 16:24 level 1 white, 25:28 level 2 white, 29: level 3 white
+// bit 32:47 level 0 black, 48:56 level 1 black, 57:60 level 2 black, 61: level 3 black
+const Move = u64;
+const InvalidMove: Move = std.math.maxInt(Move);
+const Win = Vals_max - 1;
+const Bwin = Win - 1;
+const WHITE: Colors = 0;
+const BLACK: Colors = 1;
+const NB_COLS: usize = @as(usize, @intCast(BLACK)) + 1;
+const NB_LEVELS: usize = 4;
+const Moves = [64]Move;
+const MAX_PAWNS: u64 = 15;
 
-const Move = packed struct {
-    x: usize,
-    y: usize,
-    z: usize,
-    ox: usize,
-    oy: usize,
-    oz: usize,
-    move: bool, // if true do not use (ox,oy,oz) and take the ball from the reserve
-};
-const NB_MAX_MOVES = 32;
-const Moves = [NB_MAX_MOVES]Move;
-
-const FOUR: usize = 4;
-const WHITE: Colors = 1;
-const BLACK = -WHITE;
-const EMPTY: Colors = 0;
 const HASH_SIZE: usize = 1 << NB_BITS;
 const HASH_MASK: Sigs = HASH_SIZE - 1;
-
-var first_hash: Sigs = undefined;
-var hashesw: [SIZEX][SIZEY][SIZEZ]Sigs = undefined;
-var hashesb: [SIZEX][SIZEY][SIZEZ]Sigs = undefined;
+var hashesv: [NB_COLS][NB_LEVELS][]Sigs = undefined;
+var hash_black: Sigs = undefined;
 
 const HashElem = packed struct {
     sig: Sigs,
     v_inf: Vals,
     v_sup: Vals,
-    d: Depth,
+    base: Depth,
+    dist: Depth,
+    bmove: Move,
 };
 
 const ZHASH = HashElem{
     .sig = 0,
-    .v_inf = 0,
-    .v_sup = 0,
-    .d = 0,
+    .v_inf = Vals_min,
+    .v_sup = Vals_max,
+    .base = 0,
+    .dist = 0,
+    .bmove = InvalidMove,
 };
 
 var hashes: []HashElem = undefined;
-//var tab1 = [_]Colors{EMPTY} ** SIZEZ;
-//var tab2 = [_][SIZEY]Colors{[_]Colors{EMPTY} ** SIZEY} ** SIZEX;
-var tab = [_][SIZEY][SIZEZ]Colors{[_][SIZEZ]Colors{[_]Colors{EMPTY} ** SIZEZ} ** SIZEY} ** SIZEX;
-var rems = [_]i64{15} ** 2;
 
-fn retrieve(hv: Sigs, v_inf: *Vals, v_sup: *Vals) bool {
+fn retrieve(hv: Sigs, v_inf: *Vals, v_sup: *Vals, bmove: *Move, dist: Depth) bool {
     const ind: usize = hv & HASH_MASK;
     if (hashes[ind].sig == hv) {
-        v_inf.* = hashes[ind].v_inf;
-        v_sup.* = hashes[ind].v_sup;
-        return true;
-    } else {
-        return false;
+        bmove.* = hashes[ind].bmove;
+        if (hashes[ind].dist == dist) {
+            v_inf.* = hashes[ind].v_inf;
+            v_sup.* = hashes[ind].v_sup;
+            //            return true;
+            return false;
+        }
     }
+    return false;
 }
 
-fn store(hv: Sigs, alpha: Vals, beta: Vals, g: Vals, depth: Depth) void {
+fn store(hv: Sigs, alpha: Vals, beta: Vals, g: Vals, dist: Depth, base: Depth, bmove: Move) void {
     const ind = hv & HASH_MASK;
-    const d = std.math.maxInt(Depth) - depth;
-    if (hashes[ind].d <= d) {
-        if (hashes[ind].sig != hv) {
-            hashes[ind].d = d;
+    if ((hashes[ind].base != base) or (hashes[ind].dist <= dist)) {
+        if ((hashes[ind].sig != hv) or (hashes[ind].dist != dist)) {
+            hashes[ind].dist = dist;
             hashes[ind].v_inf = Vals_min;
             hashes[ind].v_sup = Vals_max;
             hashes[ind].sig = hv;
         }
+        hashes[ind].base = base;
+        hashes[ind].bmove = bmove;
         if ((g > alpha) and (g < beta)) {
             hashes[ind].v_inf = g;
             hashes[ind].v_sup = g;
@@ -91,148 +89,165 @@ fn store(hv: Sigs, alpha: Vals, beta: Vals, g: Vals, depth: Depth) void {
     }
 }
 
-fn eval() Vals {
+fn compute_hash(m: Move, color: Colors) Sigs {
+    //    var h: Sigs = 0;
+    if (m == 0) return 0;
+    if (color == 0) return 0;
     return 0;
 }
 
-fn build_all_moves(color: Colors, moves: *Moves) usize {
-    var freex: [30]usize = undefined;
-    var freey: [30]usize = undefined;
-    var freez: [30]usize = undefined;
-    var nb: usize = 0;
-    for (0..3) |z| {
-        const nz = z + 1;
-        for (0..4 - z) |x| {
-            for (0..4 - z) |y| {
-                if (tab[x][y][z] == color) {
-                    var ok = true;
-                    outer: for (0..3) |dx| {
-                        for (0..3) |dy| {
-                            if ((dx != 1) or (dy != 1)) {
-                                const nx = x + dx - 1;
-                                const ny = y + dy - 1;
-                                if ((nx >= 0) and (nx < 4 - nz) and (ny >= 0) and (ny < 4 - nz) and (tab[nx][ny][nz] != EMPTY)) {
-                                    ok = false;
-                                    break :outer;
-                                }
-                            }
-                        }
-                    }
-                    if (ok) {
-                        freex[nb] = x;
-                        freey[nb] = y;
-                        freez[nb] = z;
-                        nb += 1;
-                    }
-                }
-            }
+var best_move: Move = undefined;
+fn updateab(color: Colors, depth: Depth, base: Depth, v: Vals, a: *Vals, b: *Vals, g: *Vals, p: u64, lmove: *Move) bool {
+    if (color == WHITE) {
+        if (v > g.*) {
+            g.* = v;
+            lmove.* = @as(Move, @intCast(p));
+            if (depth == base) best_move = lmove.*;
         }
+        a.* = @max(a.*, g.*);
+    } else {
+        if (v < g.*) {
+            g.* = v;
+            lmove.* = @as(Move, @intCast(p));
+            if (depth == base) best_move = lmove.*;
+        }
+        b.* = @min(b.*, g.*);
     }
-    moves[0].x = 0;
-    moves[0].y = 0;
-    moves[0].z = 0;
-    moves[0].ox = 0;
-    moves[0].oy = 0;
-    moves[0].oz = 0;
-    moves[0].move = true;
+    return (a.* >= b.*);
+}
 
+fn eval(m: Move, c: Colors) Vals {
+    const mt = [2]u32{ @intCast(m & 0xffffffff), @intCast(m << 32) };
+    if (@popCount(mt[c]) == MAX_PAWNS) {
+        if (c == WHITE) return -Win / 3 else return Win / 3;
+    }
     return 0;
 }
 
-fn ab(
-    alpha: Vals,
-    beta: Vals,
-    color: Colors,
-    depth: Depth,
-    maxdepth: Depth,
-    hv: Sigs,
-) Vals {
-    var a = alpha;
-    var b = beta;
+fn gen_moves(m: Move, c: Colors, t: *Moves) usize {
+    //    const mt = [2]u32{ m & 0xffffffff, m << 32 };
+    t[0] = 0;
+    if ((m == 0) and (c == 0)) return 0;
+    return 0;
+}
+
+var hit: u64 = 0;
+var nodes: u64 = 0;
+fn ab(alp: Vals, bet: Vals, color: Colors, maxdepth: Depth, depth: Depth, base: Depth, m: Move) Vals {
+    const oppcol = 1 - color;
+    if (depth == maxdepth) return eval(m, color);
+    nodes += 1;
+    var alpha = alp;
+    var beta = bet;
+    var bmove: Move = InvalidMove;
+    const hv = compute_hash(m, color);
     var v_inf: Vals = undefined;
     var v_sup: Vals = undefined;
-    if (retrieve(hv, &v_inf, &v_sup)) {
+    if (retrieve(hv, &v_inf, &v_sup, &bmove, maxdepth - depth)) {
+        if (depth == base) best_move = bmove;
         if (v_inf == v_sup) return v_inf;
-        if (v_inf >= b) return v_inf;
-        if (v_sup <= a) return v_sup;
-        a = @max(a, v_inf);
-        b = @min(b, v_sup);
-    }
-    if (tab[0][0][3] != EMPTY) {
-        return WIN * color;
-    }
-    if ((color == WHITE) and (rems[0] == 0)) {
-        return -WIN;
-    }
-    if ((color == BLACK) and (rems[1] == 0)) {
-        return WIN;
-    }
-    if (depth == maxdepth) {
-        return eval();
+        if (v_inf >= beta) return v_inf;
+        if (v_sup <= alpha) return v_sup;
+        alpha = @max(alpha, v_inf);
+        beta = @min(beta, v_sup);
+        hit += 1;
     }
 
+    if (!USE_BMOVE) bmove = InvalidMove;
+
+    var a = alpha;
+    var b = beta;
+    var lmove: Move = InvalidMove;
+
     var g: Vals = if (color == WHITE) Vals_min else Vals_max;
-    var nhv: Sigs = undefined;
-    var moves: Moves = undefined;
-    var nb: usize = build_all_moves(color, &moves);
-    for (0..nb) |i| {
-        const x = moves[i].x;
-        const y = moves[i].y;
-        const z = moves[i].z;
-        tab[x][y][z] = color;
-        if (color == WHITE) {
-            nhv = hv ^ hashesw[x][y][z];
-        } else {
-            nhv = hv ^ hashesb[x][y][z];
+    outer: {
+        if (bmove != InvalidMove) {
+            const v = ab(a, b, oppcol, maxdepth, depth + 1, base, bmove);
+            if (updateab(color, depth, base, v, &a, &b, &g, bmove, &lmove)) break :outer;
         }
-        if (moves[i].move) {
-            const ox = moves[i].x;
-            const oy = moves[i].y;
-            const oz = moves[i].z;
-            tab[ox][oy][oz] = EMPTY;
-            if (color == WHITE) {
-                nhv = hv ^ hashesw[ox][oy][oz];
-            } else {
-                nhv = hv ^ hashesb[ox][oy][oz];
-            }
+        var t: Moves = undefined;
+        const nb = gen_moves(m, color, &t);
+        for (0..nb) |i| {
+            const v = ab(a, b, oppcol, maxdepth, depth + 1, base, t[i]);
+            if (updateab(color, depth, base, v, &a, &b, &g, t[i], &lmove)) break :outer;
         }
-        const v = ab(a, b, -color, depth + 1, maxdepth, nhv);
-        tab[x][y][z] = EMPTY;
-        if (color == WHITE) {
-            g = @max(v, g);
-            a = @max(a, g);
-        } else {
-            g = @min(v, g);
-            a = @min(b, g);
-        }
-        if (a >= b) break;
     }
-    store(hv, alpha, beta, g, depth);
+    store(hv, alpha, beta, g, maxdepth - depth, base, lmove);
     return g;
 }
 
+fn print_pos(m: Move) !void {
+    try stderr.print("move={x}\n", .{m});
+}
+
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+    var args = std.process.args();
+    _ = args.next();
+    const sturn = args.next().?;
+    var turn = std.fmt.parseInt(u8, sturn, 10) catch 0;
+    if ((turn != 1) and (turn != 2)) std.posix.exit(255);
+
     const allocator = std.heap.page_allocator;
-    const RndGen = std.rand.DefaultPrng;
+    const RndGen = std.Random.DefaultPrng;
     hashes = try allocator.alloc(HashElem, HASH_SIZE);
     defer allocator.free(hashes);
     for (hashes) |*a| a.* = ZHASH;
     var rnd = RndGen.init(0);
-    for (0..SIZEX) |x| {
-        for (0..SIZEY) |y| {
-            for (0..SIZEZ) |z| {
-                hashesw[x][y][z] = rnd.random().int(Sigs);
-                hashesb[x][y][z] = rnd.random().int(Sigs);
+    for (0..NB_COLS) |i| {
+        for (0..NB_LEVELS) |j| {
+            const one: usize = 1;
+            const nj: u6 = @as(u6, @intCast(j * j));
+            const nb: usize = one << nj;
+            hashesv[i][j] = try allocator.alloc(Sigs, nb);
+            for (0..nb) |k| {
+                hashesv[i][j][k] = rnd.random().int(Sigs);
             }
         }
     }
-    first_hash = rnd.random().int(Sigs);
-    var t = std.time.milliTimestamp();
-    const ret = ab(Vals_min, Vals_max, WHITE, 0, 1, first_hash);
-    t = std.time.milliTimestamp() - t;
-    try stdout.print("{d}\n", .{t});
-    try stdout.print("{d}\n", .{ret});
+    hash_black = rnd.random().int(Sigs);
+
+    var base: Depth = 0;
+    var t: i64 = undefined;
+    var ret: Vals = undefined;
+    var buf: [1000]u8 = undefined;
+    var oppmove: Move = undefined;
+    var color: Colors = undefined;
+    var maxdepth: Depth = undefined;
+    var m: Move = 0;
+    color = if (turn == 1) WHITE else BLACK;
+    while (true) {
+        if (turn == 1) {
+            var total_time: i64 = 0;
+            maxdepth = base + 1;
+            ret = 0;
+            while ((total_time < 2000) and (@abs(ret) < Bwin)) {
+                best_move = InvalidMove;
+                t = std.time.milliTimestamp();
+                hit = 0;
+                nodes = 0;
+                ret = ab(Vals_min, Vals_max, color, maxdepth, base, base, m);
+                if (best_move == InvalidMove) break;
+                t = std.time.milliTimestamp() - t;
+                total_time += t;
+                try stderr.print("depth={d} t={d}ms ret={d} nodes={d} hit={d} best_move={d}\n", .{ maxdepth - base, t, ret, nodes, hit, best_move });
+                maxdepth += 1;
+            }
+            try print_pos(best_move);
+            m = best_move;
+            base += 1;
+            color = if (color == WHITE) BLACK else WHITE;
+        }
+        turn = 1;
+        while (true) {
+            try stderr.print("Your move:", .{});
+            if (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |v| oppmove = std.fmt.parseInt(Move, v, 10) catch InvalidMove;
+            m = oppmove;
+            break;
+        }
+        try print_pos(oppmove);
+        base += 1;
+        color = if (color == WHITE) BLACK else WHITE;
+    }
 }
 
 //const Inner = struct { a: u32, b: bool };
